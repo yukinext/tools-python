9d802073d2212694ffa64d63fdd16074805dca6a
yukinext <yuki.next@gmail.com>
2019-08-25 10:51:00 +0900

    add pattern 'TSC おばあちゃんの台所"

--- scraper/recipe/main.py
+++ scraper/recipe/main.py
@@ -101,7 +101,10 @@ class EvernoteTransrator(object):
 <ul>
 {%- for recipe_step in recipe.recipe_steps %}
     <li>
-        <div>{{ recipe_step.text }}</div>
+        {%- set recipe_step_ls = recipe_step.text.splitlines() %}
+        {%- for recipe_step_l in recipe_step_ls %}
+        <div>{{ recipe_step_l }}</div>
+        {%- endfor %}
 {%- for image_url in recipe_step.image_urls %}
         <br /><en-media type="{{ image_resources[image_url].mime }}" hash="{{ image_resources[image_url].data.bodyHash }}" />
 {%- endfor %}        
@@ -112,7 +115,6 @@ class EvernoteTransrator(object):
 {%- endfor %}
 </ul>
 {%- endif %}
-
 {%- endautoescape %}
 </en-note>
 """
@@ -246,7 +248,7 @@ class RecipeCrawlerTemplate(object):
         
         recipes = dict() # key: Recipe.id, value: Recipe
 
-        for entry_url in self.entry_urls:
+        for entry_url in self._expand_entry_urls():
             res = requests.get(entry_url, verify=False)
             if res.ok:
                 soup = BeautifulSoup(res.content, "html5lib", from_encoding=res.apparent_encoding)
@@ -300,6 +302,9 @@ class RecipeCrawlerTemplate(object):
                 new_target_fn = self._get_new_fn(target_fn, "_", 1)
                 logger.info("{}: rename : {} -> {}".format(self.__class__.site_name, target_fn.name, new_target_fn.name))
                 target_fn.rename(new_target_fn)
+    
+    def _expand_entry_urls(self):
+        return self.entry_urls
     
     def _is_existed_recipe(self, recipe):
         assert isinstance(recipe, Recipe)
@@ -1021,6 +1026,74 @@ class NhkKiichiRecipeCrawler(RecipeCrawlerTemplate):
         recipe = copy.deepcopy(overview_recipe)
 
         yield recipe
+
+
+class TscGrandmotherKitchenRecipeCrawler(RecipeCrawlerTemplate):
+    site_name = "tsc_grandmother_kitchen"
+
+    def _expand_entry_urls(self):
+        ret = set()
+        for entry_url in self.entry_urls:
+            res = requests.get(entry_url)
+            if res.ok:
+                soup = BeautifulSoup(res.content, "html5lib", from_encoding=res.apparent_encoding)
+                last_page_url = soup.find("div", "pagination").find_all("a")[-1]["src"]
+                page_url, last_page_num_str = re.search(r"(.*)/(\d+)/$", last_page_url).groups()
+                
+                for page_num in range(1, int(last_page_num_str) + 1):
+                    ret.add("{}/{}/".format(page_url, page_num))
+            
+        return ret
+
+    def _get_recipe_overviews(self, overview_soup, entry_url):
+        recipes = dict() # key: Recipe.id, value: Recipe
+        for item in overview_soup.find_all("article", "post-archive"):
+            recipe = Recipe()
+            
+            cns_buf = [l.replace("「","").replace("」", "") for l in reversed(item.h2.get_text("\n").splitlines())]
+            cns_buf.insert(1, item.find("div", "recipe-archive-cast").text)
+            
+            recipe.cooking_name_sub = "/".join(cns_buf)
+            recipe.detail_url = item.a["href"]
+            recipe.id = int(re.search(r"/(\d+)\D*$", recipe.detail_url).group(1))
+            recipe.program_name = self.program_name
+            recipe.program_date = datetime.date(*[int(s) for s in re.search(r"(\d+)\D+(\d+)\D+(\d+)\D+", item.find("div", "recipe-onairs").text).groups()])
+            recipes[recipe.id] = recipe
+
+        return recipes
+    
+    def _recipe_details_generator(self, detail_soup, overview_recipe):
+        """
+        must deepcopy "recipe" before use
+        """
+        for recipe_box_node in detail_soup.find_all("section", "recipe-box"):
+            if recipe_box_node.h3 is None:
+                continue
+
+            recipe = copy.deepcopy(overview_recipe)
+            recipe.cooking_name = recipe_box_node.h3.text.replace("「", "").replace("」", "")
+            recipe.image_urls.append(detail_soup.find("div", "recipe-food-img").img["src"])
+    
+            material_title_node = recipe_box_node.find("div", "recipe-material")
+            m_buf = []
+            m_buf.append("（{}）".format(material_title_node.span.text))
+            for dt in material_title_node.find_all("dt"):
+                material_title = dt.text.replace("材料", "").replace("(", "（").replace(")", "）").strip()
+                if len(material_title):
+                    m_buf.append(material_title)
+                dd = dt.find_next_sibling("dd")
+                for material in [": ".join([td.text.strip() for td in tr.find_all("td")]) for tr in dd.find_all("tr")]:
+                    if len(material.strip()):
+                        m_buf.append("　" + material)
+            recipe.materials.extend([RecipeText(m) for m in m_buf])
+            
+            for i, recipe_step in enumerate(recipe_box_node.find("ol", "recipe-process-list").find_all("li")):
+                recipe_step_str = recipe_step.text.strip()
+                if len(recipe_step_str):
+                    recipe.recipe_steps.append(RecipeText("（{}）{}".format(i + 1, recipe_step_str)))
+            
+            yield recipe
+
         
 def store_evernote(recipes, args, site_config, evernote_cred, is_note_exist_check=True):
     client = EvernoteClient(token=evernote_cred["developer_token"], sandbox=evernote_cred["is_sandbox"])
@@ -1183,6 +1256,7 @@ def main():
             NhkKobaraSuitemasenkaRecipeCrawler(),
             NhkKobaraGaSukimashitaRecipeCrawler(),
             NhkKiichiRecipeCrawler(),
+            TscGrandmotherKitchenRecipeCrawler(),
             ]])
 
     config = yaml.safe_load(args.config_yaml_filename.open("r").read())
